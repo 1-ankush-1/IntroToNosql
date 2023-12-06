004c6b025a93cca170917219eaccb07e51b2a109
Ankush <ankushsharma9685@gmail.com>
2023-12-06 18:41:52 +0530

    Established connection with mongo

--- util/database.js
+++ util/database.js
@@ -1,8 +1,13 @@
-const Sequelize = require('sequelize');
+const { MongoClient } = require('mongodb');
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+exports.DbConnect = () => {
+  MongoClient.connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_CLUSTER}.0cghwlb.mongodb.net/`)
+    .then(client => {
+      console.log("connected successfully");
+    }).catch(err => console.log(err));
+}
 
-module.exports = sequelize;
+
+
+
