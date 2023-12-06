727cf3c9b46ab783bca21cd5e17eda8c58d792fb
Ankush <ankushsharma9685@gmail.com>
2023-12-06 18:57:58 +0530

    client can perform action probl - here callback on every request db connection ahappen

--- models/product.js
+++ models/product.js
@@ -1,27 +1,18 @@
-const Sequelize = require('sequelize');
+const { DbConnect } = require("../util/database");
 
-const sequelize = require('../util/database');
-
-const Product = sequelize.define('product', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  title: Sequelize.STRING,
-  price: {
-    type: Sequelize.DOUBLE,
-    allowNull: false
-  },
-  imageUrl: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  description: {
-    type: Sequelize.STRING,
-    allowNull: false
+class Product {
+  constructor(title, price, imageUrl, description) {
+    this.title = title;
+    this.price = price;
+    this.imageUrl = imageUrl;
+    this.description = description;
   }
-});
+
+  save() {
+    DbConnect(client=>{
+      console.log(client);
+    })
+  }
+}
 
 module.exports = Product;
