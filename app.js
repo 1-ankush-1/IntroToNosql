7594a73095de596ac1c957ce8e84fc46a14050e1
Ankush <ankushsharma9685@gmail.com>
2023-12-07 15:44:17 +0530

    user can add single item in cart

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.fetchById('6571830d24be986496b6a41a').then(user => {
     // console.log(user);
-    req.user = user;
+    req.user = new User(user.name, user.email, user.cart, user._id);
     next();
   }).catch(err => {
     console.log(err)
--- models/user.js
+++ models/user.js
@@ -2,15 +2,29 @@ const { dbInstance } = require("../util/database");
 const mongodb = require("mongodb");
 
 class User {
-  constructor(name, email, id) {
+  constructor(name, email, cart, id) {
     this.name = name;
     this.email = email;
+    this.cart = cart;
     this._id = id;
   }
 
   save() {
     const db = dbInstance();
     return db.collection("users").insertOne(this);
+  }
+
+  addToCart(product) {
+    const db = dbInstance();
+    // const cartProducts = this.cart.items.findIndex(cp=>{
+    //   return cp._id === product.id;
+    // })
+
+    const updateCart = { items: [{ productId: new mongodb.ObjectId(product._id), quantity: 1 }] };
+
+    return db.collection('users').updateOne(
+      { _id: new mongodb.ObjectId(this._id) },
+      { $set: { cart: updateCart } });
   }
 
   static fetchById(userId) {
