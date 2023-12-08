bb660be71efb9eea557483c327c2df5270dbd7f4
Ankush <ankushsharma9685@gmail.com>
2023-12-08 10:34:43 +0530

    made connection to with mongoose

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
 
 const errorController = require('./controllers/error');
 const app = express();
@@ -11,30 +12,29 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const { DbConnect } = require('./util/database');
 const User = require("./models/user");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  User.fetchById('6571830d24be986496b6a41a').then(user => {
-    // console.log(user);
-    req.user = new User(user.name, user.email, user.cart, user._id);
-    next();
-  }).catch(err => {
-    console.log(err)
-    next();
-  });
-})
+// app.use((req, res, next) => {
+//   User.fetchById('6571830d24be986496b6a41a').then(user => {
+//     // console.log(user);
+//     req.user = new User(user.name, user.email, user.cart, user._id);
+//     next();
+//   }).catch(err => {
+//     console.log(err)
+//     next();
+//   });
+// })
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-DbConnect(() => {
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_CLUSTER}.0cghwlb.mongodb.net/`).then(result => {
   app.listen(3000, () => {
     console.log("server started");
   })
-});
+}).catch(err => console.log(err));
