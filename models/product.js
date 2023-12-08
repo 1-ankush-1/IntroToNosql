a387a362dbcce595400a5750c020bf080ed6745f
Ankush <ankushsharma9685@gmail.com>
2023-12-08 10:37:50 +0530

    created product schema and imported its model

--- models/product.js
+++ models/product.js
@@ -1,72 +1,93 @@
-const { dbInstance } = require("../util/database");
-const mongodb = require("mongodb");
+const mongoose = require("mongoose");
 
-class Product {
-  constructor(title, price, imageUrl, description, id,userId) {
-    this.title = title;
-    this.price = price;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this._id = id;
-    this.userId = userId;
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  imageUrl: {
+    type: String,
   }
+});
 
-  save() {
-    const db = dbInstance();                                      //perform operations on respective db
-    let dbQuery;
-    // console.log(this);
-    // return;
-    if (this._id) {                                          //update old
-      dbQuery = db.collection('products').updateOne({ _id: this._id }, { $set: this });
-    } else {                                                 //new create
-      dbQuery = db.collection('products').insertOne(this)
-    }
-    //this refer to current object of product class
-    return new Promise((resolve, reject) => {
-      dbQuery.then(result => {    //perform operation on which collection (on new operation it will create collection if not exist)
-        resolve(result);
-      }).catch(err => {
-        console.log(err);
-        reject(err);
-      })
-    })
-  }
+module.exports = mongoose.model('Product', productSchema);
 
-  fetchAll() {
-    const db = dbInstance();
-    return new Promise((resolve, reject) => {
-      db.collection('products').find().toArray().then(result => {    //because find doesnt return a promise so using toArrray
-        resolve(result);
-      }).catch(err => {
-        console.log(err);
-        reject(err);
-      })
-    })
-  }
+// class Product {
+//   constructor(title, price, imageUrl, description, id,userId) {
+//     this.title = title;
+//     this.price = price;
+//     this.imageUrl = imageUrl;
+//     this.description = description;
+//     this._id = id;
+//     this.userId = userId;
+//   }
 
-  fetchById(prodId) {
-    const db = dbInstance();
-    return new Promise((resolve, reject) => {
-      db.collection('products').find({ _id: new mongodb.ObjectId(prodId) }).next().then(result => {    //used objectId because of tyoe BSON because find doesnt return a promise so using next to get document
-        resolve(result);
-      }).catch(err => {
-        console.log(err);
-        reject(err);
-      })
-    })
-  }
+//   save() {
+//     const db = dbInstance();                                      //perform operations on respective db
+//     let dbQuery;
+//     // console.log(this);
+//     // return;
+//     if (this._id) {                                          //update old
+//       dbQuery = db.collection('products').updateOne({ _id: this._id }, { $set: this });
+//     } else {                                                 //new create
+//       dbQuery = db.collection('products').insertOne(this)
+//     }
+//     //this refer to current object of product class
+//     return new Promise((resolve, reject) => {
+//       dbQuery.then(result => {    //perform operation on which collection (on new operation it will create collection if not exist)
+//         resolve(result);
+//       }).catch(err => {
+//         console.log(err);
+//         reject(err);
+//       })
+//     })
+//   }
 
-  static deleteById(prodId) {
-    const db = dbInstance();
-    return new Promise((resolve, reject) => {
-      db.collection('products').deleteOne({ _id: new mongodb.ObjectId(prodId) }).then(result => {    //used objectId because of tyoe BSON because find doesnt return a promise so using next to get document
-        resolve("deleted");
-      }).catch(err => {
-        console.log(err);
-        reject(err);
-      })
-    })
-  }
-}
+//   fetchAll() {
+//     const db = dbInstance();
+//     return new Promise((resolve, reject) => {
+//       db.collection('products').find().toArray().then(result => {    //because find doesnt return a promise so using toArrray
+//         resolve(result);
+//       }).catch(err => {
+//         console.log(err);
+//         reject(err);
+//       })
+//     })
+//   }
 
-module.exports = Product;
+//   fetchById(prodId) {
+//     const db = dbInstance();
+//     return new Promise((resolve, reject) => {
+//       db.collection('products').find({ _id: new mongodb.ObjectId(prodId) }).next().then(result => {    //used objectId because of tyoe BSON because find doesnt return a promise so using next to get document
+//         resolve(result);
+//       }).catch(err => {
+//         console.log(err);
+//         reject(err);
+//       })
+//     })
+//   }
+
+//   static deleteById(prodId) {
+//     const db = dbInstance();
+//     return new Promise((resolve, reject) => {
+//       db.collection('products').deleteOne({ _id: new mongodb.ObjectId(prodId) }).then(result => {    //used objectId because of tyoe BSON because find doesnt return a promise so using next to get document
+//         resolve("deleted");
+//       }).catch(err => {
+//         console.log(err);
+//         reject(err);
+//       })
+//     })
+//   }
+// }
+
+
